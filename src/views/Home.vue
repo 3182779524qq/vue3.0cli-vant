04634b19693795e44d21688f473c740c475a6950
3182779524qq <3182779524@qq.com>
2019-12-16 10:04:53 +0800

    格式化代码

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,11 @@
   <div class="home">
     <h1>开始搭建项目啦，哈哈哈</h1>
     <van-grid>
-      <van-grid-item icon="photo-o" text="文字"/>
-      <van-grid-item icon="photo-o" text="文字"/>
-      <van-grid-item icon="photo-o" text="文字"/>
-      <van-grid-item icon="photo-o" text="文字"/>
-    </van-grid>
+      <van-grid-item icon="photo-o" text="文字" />
+      <van-grid-item icon="photo-o" text="文字" />
+      <van-grid-item icon="photo-o" text="文字" />
+      <van-grid-item icon="photo-o" text="文字" />
+      </van-grid>
     <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
@@ -17,17 +17,17 @@
 // import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: "home",
+  name: 'home',
   components: {
     // HelloWorld
   }
-};
+}
 </script>
 <style scoped>
-h1 {
-  font-size: 34px;
-  font-family: Source Han Sans CN;
-  font-weight: 500;
-  /* border-bottom: 1px solid #000; */
-}
+  h1{
+    font-size:34px;
+    font-family:Source Han Sans CN;
+    font-weight:500;
+    /* border-bottom: 1px solid #000; */
+  }
 </style>
